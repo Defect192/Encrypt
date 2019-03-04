be73532a1e3ce2b68d86b84675c8a9d1fcbfba05
Defect192 <eaheaton0@gmail.com>
2019-03-04 10:30:22 -0700

    GUI for testing encryption

---
+++ app/src/main/java/com/example/encrypt_sms/Encryptor.java
@@ -0,0 +1,12 @@
+package com.example.encrypt_sms;
+
+public class Encryptor {
+
+    private Key PrivateKey;
+    private Key PublicKey;
+
+    public Encryptor(){
+
+    }
+
+}
---
+++ app/src/main/java/com/example/encrypt_sms/Key.java
@@ -0,0 +1,14 @@
+package com.example.encrypt_sms;
+
+import java.math.BigInteger;
+
+public class Key {
+
+    private BigInteger e;
+    private BigInteger n;
+    private BigInteger d;
+
+    public Key(){
+
+    }
+}
