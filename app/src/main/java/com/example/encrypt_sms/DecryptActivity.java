6ab99e40926c522ce666cb4c2ad040bcd9d53d82
Defect192 <eaheaton0@gmail.com>
2019-04-16 22:37:25 -0600

    GUI is complete, encryption is not

--- app/src/main/java/com/example/encrypt_sms/DecryptActivity.java
+++ app/src/main/java/com/example/encrypt_sms/DecryptActivity.java
@@ -6,8 +6,21 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.math.BigInteger;
 
 public class DecryptActivity extends AppCompatActivity {
+
+    private Button Decrypt;
+    private Button Back;
+    private EditText EnterKey;
+    private EditText EnterMessage;
+    private TextView DisplayMessage;
+
+    private Encryptor decryptMachine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +28,38 @@ public class DecryptActivity extends AppCompatActivity {
         setContentView(R.layout.activity_decrypt);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Decrypt = (Button)findViewById(R.id.DecryptButton);
+        Back = (Button)findViewById(R.id.back2);
+        EnterKey = (EditText)findViewById(R.id.EnterKey);
+        EnterMessage = (EditText)findViewById(R.id.PasteMessage);
+        DisplayMessage = (TextView)findViewById(R.id.Message);
+
+
+        Decrypt.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                decryptMessage(v);
+            }
+        });
+
+        Back.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        decryptMachine= new Encryptor(false);
+
+
     }
+
+    public void decryptMessage(View v){
+        if(EnterMessage.getText().length()!=0){
+            String Decoded= decryptMachine.Decode(EnterMessage.getText().toString(),decryptMachine.getMyKeys().getPublicKey());
+            DisplayMessage.setText(Decoded);
+        }
+
+    }
+
 
 }
--- app/src/main/java/com/example/encrypt_sms/KeyActivity.java
+++ app/src/main/java/com/example/encrypt_sms/KeyActivity.java
@@ -1,13 +1,21 @@
 package com.example.encrypt_sms;
 
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class KeyActivity extends AppCompatActivity {
+
+    private Button backbutton;
+    private Button addbutton;
+    private Button deletebutton;
+    private EditText enterkeyname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +23,38 @@ public class KeyActivity extends AppCompatActivity {
         setContentView(R.layout.activity_key);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        enterkeyname= (EditText)findViewById(R.id.enterKeyName);
+
+        backbutton= (Button)findViewById(R.id.back3);
+        backbutton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        addbutton= (Button)findViewById(R.id.add);
+        addbutton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                addKey(v);
+            }
+        });
+
+        deletebutton= (Button)findViewById(R.id.delete);
+        deletebutton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                eraseKey(v);
+            }
+        });
     }
+
+    public void addKey(View v){
+
+    }
+
+    public void eraseKey(View v){
+
+    }
+
 
 }
